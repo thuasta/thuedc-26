23fec614bf8483495d78342184d5d95a88a29582
LxCoding <2176427177@qq.com>
2024-09-20 15:55:49 +0800

    update gui.py and add src(qml)

--- edc_frontend/edc_frontend/gui.py
+++ edc_frontend/edc_frontend/gui.py
@@ -1,10 +1,42 @@
+import sys
+
+from pathlib import Path
+
+from PySide6.QtCore import QObject, Slot
+from PySide6.QtQuickControls2 import QQuickStyle
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine, QmlElement
+
+QML_IMPORT_NAME = "io.qt.edcviewer"
+QML_IMPORT_MAJOR_VERSION = 1
+
+@QmlElement
+class Bridge(QObject):
+
+    @Slot(str, result = bool)
+    def register(self, s):
+        if s.lower() == "12345678":
+            return True
+        else:
+            return False
+
 class GUI:
     """GUI class for the application"""
     def __init__(self):
-        pass
+        self.app = QGuiApplication(sys.argv)
+        self.engine = QQmlApplicationEngine()
+
+        self.qml_file = Path(__file__).parent / 'src' / 'View.qml'
 
     def start(self):
         """Start the GUI"""
+        QQuickStyle.setStyle("Material")
+        self.engine.load(self.qml_file)
+
+        if not self.engine.rootObjects():
+            sys.exit(-1)
+    
+        sys.exit(self.app.exec())
 
     def stop(self):
         """Stop the GUI"""
@@ -14,3 +46,8 @@ class GUI:
 
     def calibrate(self):
         """Calibrate the camera"""
+
+if __name__ == '__main__':
+    gui = GUI()
+    gui.start()
+
